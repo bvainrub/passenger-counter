098d1c13886237510f2694a4e5137f252524a69a
Boris Vainrub <43561061+bvainrub@users.noreply.github.com>
2024-06-30 20:37:00 -0300

    Update index.js
    added comments to function

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
-
+//increment passenger by one
 function increment() {
     count += 1
     countEl.textContent = count
 }
-
+//save curent batch
 function save() {
     let countStr = count + " - "
     saveEl.textContent += countStr
